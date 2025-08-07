abe2dff5d40d96b2cca36c7c986b8f68ec58d9b2
Aaron Dewes <aaron@nirvati.org>
2025-08-08 00:12:08 +0200

    fix: Download trivy

--- make/photon/trivy-adapter/builder.sh
+++ make/photon/trivy-adapter/builder.sh
@@ -2,7 +2,8 @@
 
 set +e
 
-VERSION="v0.33.2"
+VERSION="0.64.1"
+ADAPTER_VERSION="v0.33.2"
 GOBUILDIMAGE="golang:1.24.6"
 
 set -e
@@ -10,10 +11,14 @@ set -e
 cd $(dirname $0)
 cur=$PWD
 
+echo "Downloading static Trivy binary ..."
+STATIC_BINARY_URL="https://github.com/aquasecurity/trivy/releases/download/v$VERSION/trivy_${VERSION}_Linux-64bit.tar.gz"
+mkdir -p binary
+curl -L $STATIC_BINARY_URL | tar -xz -C binary
 # The temporary directory to clone Trivy adapter source code
 TEMP=$(mktemp -d ${TMPDIR-/tmp}/trivy-adapter.XXXXXX)
 git clone https://github.com/goharbor/harbor-scanner-trivy.git $TEMP
-cd $TEMP; git checkout $VERSION; cd -
+cd $TEMP; git checkout $ADAPTER_VERSION; cd -
 
 echo "Building Trivy adapter binary ..."
 cp Dockerfile.binary $TEMP
@@ -21,7 +26,7 @@ docker buildx build --build-arg golang_image=$GOBUILDIMAGE -f $TEMP/Dockerfile.b
 
 echo "Copying Trivy adapter binary from the container to the local directory..."
 ID=$(docker create trivy-adapter-golang)
-docker cp $ID:/go/src/github.com/goharbor/harbor-scanner-trivy/scanner-trivy binary
+docker cp $ID:/go/src/github.com/goharbor/harbor-scanner-trivy/scanner-trivy binary/scanner-trivy
 
 docker rm -f $ID
 docker rmi -f trivy-adapter-golang
