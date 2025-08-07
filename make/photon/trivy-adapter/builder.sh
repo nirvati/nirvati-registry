7156e99843df55109c7aec627b29108acf73dff8
Aaron Dewes <aaron@nirvati.org>
2025-08-07 23:54:55 +0200

    Load trivy image to Docker

--- make/photon/trivy-adapter/builder.sh
+++ make/photon/trivy-adapter/builder.sh
@@ -17,7 +17,7 @@ cd $TEMP; git checkout $VERSION; cd -
 
 echo "Building Trivy adapter binary ..."
 cp Dockerfile.binary $TEMP
-docker buildx build --build-arg golang_image=$GOBUILDIMAGE -f $TEMP/Dockerfile.binary -t trivy-adapter-golang $TEMP
+docker buildx build --build-arg golang_image=$GOBUILDIMAGE -f $TEMP/Dockerfile.binary -t trivy-adapter-golang --load $TEMP
 
 echo "Copying Trivy adapter binary from the container to the local directory..."
 ID=$(docker create trivy-adapter-golang)
